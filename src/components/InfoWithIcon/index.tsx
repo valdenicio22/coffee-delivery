b831d566bf3bb749d3fb6bca91268d9f663ac218
valdenicio22 <valdenicio22@gmail.com>
2023-07-17 09:21:20 +0200

    feat: create infoWithIcon component

---
+++ src/components/InfoWithIcon/index.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import { RegularText } from '../Typography'
+
+interface InfoWithIconProps {
+  info: string | ReactNode
+  icon: ReactNode
+  iconBgColor: 'base-text' | 'yellow-dark' | 'yellow' | 'purple'
+  iconColor?: 'background' | 'yellow-dark' | 'purple'
+}
+
+export function InfoWithIcon({
+  info,
+  icon,
+  iconColor = 'background',
+  iconBgColor,
+}: InfoWithIconProps) {
+  const iconBgColorVarient = {
+    'base-text': 'bg-base-text',
+    'yellow-dark': 'bg-brand-yellow-dark',
+    yellow: 'bg-brand-yellow',
+    purple: 'bg-brand-purple',
+  }
+  const iconColorVarient = {
+    background: 'text-base-background',
+    'yellow-dark': 'text-brand-yellow-dark',
+    purple: 'text-brand-purple',
+  }
+  return (
+    <div className={`flex items-center gap-3`}>
+      <div
+        className={`w-8 h-8 rounded-full p-2 ${iconBgColorVarient[iconBgColor]} ${iconColorVarient[iconColor]}`}
+      >
+        {icon}
+      </div>
+      {typeof info === 'string' ? <RegularText>{info}</RegularText> : info}
+    </div>
+  )
+}
