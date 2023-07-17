a8bc01892fc0442da203c2b090b2a17b6b949d57
valdenicio22 <valdenicio22@gmail.com>
2023-07-17 09:21:41 +0200

    feat: create Intro component

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,23 +1,9 @@
-import introImage from '../../assets/intro-img.svg'
-import { RegularText, TitleText } from '../../components/Typography'
+import { Intro } from './components/Intro'
 
 export const LandingPage = () => {
   return (
-    <div className={`border border-red-400`}>
-      <div
-        className={`layoutContainer flex items-center justify-between gap-9 my-24`}
-      >
-        <div>
-          <TitleText weight="extrabold">
-            Encontre o café perfeito para qualquer hora do dia
-          </TitleText>
-          <RegularText size="lg">
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </RegularText>
-        </div>
-        <img src={introImage} alt="intro-img" />
-      </div>
-    </div>
+    <section className={``}>
+      <Intro />
+    </section>
   )
 }
